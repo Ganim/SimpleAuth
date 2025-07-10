6617f050e41b3baa86bfd4bbe173a5267ee10b73
Guilherme <guilhermeganim@hotmail.com>
2025-07-10 14:40:28 -0300

    fix: eslint and prettier config

--- auth-api/eslint.config.mjs
+++ auth-api/eslint.config.mjs
@@ -1,19 +1,28 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import { defineConfig, globalIgnores } from "eslint/config";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-
+import js from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error"
-  }},
-  globalIgnores(["build/**/*"], "Ignore Build Directory"),
-  globalIgnores(["node_modules/*"], "Ignore Node Modules Directory"),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
+  globalIgnores(['build/**/*'], 'Ignore Build Directory'),
+  globalIgnores(['node_modules/*'], 'Ignore Node Modules Directory'),
   tseslint.configs.recommended,
-  eslintConfigPrettier
+  eslintPluginPrettierRecommended,
 ]);
