b066a026d3d468e2ba0be04ab81507016417482b
Guilherme <guilhermeganim@hotmail.com>
2025-07-12 20:37:53 -0300

    feat: implements JWT and refresh Token

--- auth-api/src/http/controllers/authenticate.ts
+++ auth-api/src/http/controllers/authenticate.ts
@@ -17,13 +17,27 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase();
 
-    await authenticateUseCase.execute({ email, password });
+    const { user } = await authenticateUseCase.execute({ email, password });
+
+    const token = await reply.jwtSign({}, { sign: { sub: user.id } });
+
+    const refreshToken = await reply.jwtSign(
+      {},
+      { sign: { sub: user.id, expiresIn: '7d' } },
+    );
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send(error.message);
     }
     throw error;
   }
-
-  return reply.status(200).send();
 }
--- auth-api/src/http/routes.ts
+++ auth-api/src/http/routes.ts
@@ -1,8 +1,11 @@
 import { app } from '@/app';
 import { authenticate } from './controllers/authenticate';
 import { createUser } from './controllers/create-user';
+import { refresh } from './controllers/refresh';
 
 export async function appRoutes() {
   app.post('/users', createUser);
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 }
