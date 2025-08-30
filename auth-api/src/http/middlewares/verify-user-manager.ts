b5d0353555c0b5646abb4389945403f770d047ac
Guilherme <guilhermeganim@hotmail.com>
2025-08-30 20:35:36 -0300

    patch: update middlewares

--- auth-api/src/http/middlewares/verify-user-manager.ts
+++ auth-api/src/http/middlewares/verify-user-manager.ts
@@ -1,12 +1,10 @@
+import { ForbiddenError } from '@/@errors/use-cases/forbidden-error';
 import type { FastifyRequest } from 'fastify';
 
 export async function verifyUserManager(request: FastifyRequest) {
   const { role } = request.user;
 
   if (role !== 'ADMIN' && role !== 'MANAGER') {
-    const { ForbiddenError } = await import(
-      '@/@errors/use-cases/forbidden-error'
-    );
     throw new ForbiddenError('Only MANAGER or ADMIN can perform this action');
   }
 }
