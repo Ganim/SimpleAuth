c8df44410a00ae36bb7ca03e60a93236944a307c
Guilherme <guilhermeganim@hotmail.com>
2025-08-30 20:38:09 -0300

    patch: remove refresh token

--- auth-api/src/http/controllers/core/auth/routes.ts
+++ auth-api/src/http/controllers/core/auth/routes.ts
@@ -1,11 +1,9 @@
 import { app } from '@/app';
 
 import { authenticateWithPassword } from './authenticate-with-password';
-import { refreshToken } from './refresh-token';
 import { registerNewUser } from './register-new-user';
 
 export async function authRoutes() {
   app.register(authenticateWithPassword);
   app.register(registerNewUser);
-  app.register(refreshToken);
 }
